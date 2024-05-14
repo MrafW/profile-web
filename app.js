a6d2d309276f2f8c3c61f8daeaf42b357d6176a8
mrafiw <mrafiw37@gmail.com>
2024-05-14 15:49:28 +0700

    First commit

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-
-const PORT = 8000
-const HOST = '0.0.0.0'
+//const HOST = '0.0.0.0'
+//const path = require('path'); 
 
 const app = express();
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
 
-app.get('', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/css', express.static(__dirname + 'public/css'));
+//app.use('', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index')
 })
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Server is up and listening at port: ${PORT}`)
+})
